e4c3c6b6abc6cce008fa92d5255337781175b4cc
Bryan H Cruz <bryanhcruz@outlook.com>
2024-12-25 11:34:07 -0300

    add options to limit strength of password

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -1,23 +1,36 @@
 import { Command } from 'commander';
-import { generatePassword } from '../utils/generatePassword';
+import { generatePassword, IPasswordOptions } from '../utils/generatePassword';
 import { styledLog } from '../utils/styledLog';
 
 const generateCommand = new Command('generate')
   .description('Gera uma senha forte baseada no número de caracteres')
-  .option('-l, --length <number>', 'Quantidade de caracteres para a senha')
-  .action((options) => {
-    if (options.length) {
-      const lengthAsNumber = parseInt(options.length, 10);
+  .argument('<length>', 'Quantidade de caracteres para a senha')
+  .option('-nn, --no-numbers', 'Remove números da senha')
+  .option('-ns, --no-symbols', 'Remove símbolos da senha')
+  .option('-nu, --no-uppercase', 'Remove letras maiúsculas da senha')
+  .action((length, options) => {
+    if (length) {
+      const passwordLength = parseInt(length, 10);
+      const passwordOptions: IPasswordOptions = {
+        useLowercase: true,
+        useUppercase: options.uppercase,
+        useNumbers: options.numbers,
+        useSymbols: options.symbols,
+      };
 
-      if (isNaN(lengthAsNumber) || lengthAsNumber <= 0) {
+      if (isNaN(passwordLength) || passwordLength <= 0) {
         styledLog('Por favor, forneça um número válido maior que 0.', 'error');
         return;
       }
 
-      const password = generatePassword(lengthAsNumber);
-      styledLog(`Sua senha gerada é:\n ${password}`, 'success');
+      const password = generatePassword(passwordLength, passwordOptions);
+      if (password !== '')
+        styledLog(`Sua senha gerada é:\n ${password}`, 'success');
     } else
-      styledLog('Por favor, forneça um número de caracteres para a senha.', 'warn');
+      styledLog(
+        'Por favor, forneça um número de caracteres para a senha.',
+        'warn'
+      );
   });
 
 export { generateCommand };
--- src/utils/generatePassword.ts
+++ src/utils/generatePassword.ts
@@ -1,8 +1,36 @@
-export const generatePassword = (length: number): string => {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?';
-  let password = '';
+import { styledLog } from './styledLog';
 
+export interface IPasswordOptions {
+  useLowercase?: boolean;
+  useUppercase?: boolean;
+  useNumbers?: boolean;
+  useSymbols?: boolean;
+}
+
+export const generatePassword = (
+  length: number,
+  options: IPasswordOptions = {}
+): string => {
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#%^*()-_=+[]{}|;:,.<>?';
+
+  let characters = '';
+  if (options?.useLowercase) characters += lowercaseChars;
+  if (options?.useUppercase) characters += uppercaseChars;
+  if (options?.useNumbers) characters += numberChars;
+  if (options?.useSymbols) characters += symbolChars;
+
+  if (characters.length === 0) {
+    styledLog(
+      'Por favor, forneça ao menos um tipo de caractere para a senha.',
+      'error'
+    );
+    return '';
+  }
+
+  let password = '';
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     password += characters[randomIndex];
